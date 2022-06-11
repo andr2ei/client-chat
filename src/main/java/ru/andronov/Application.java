9712a691d2db1306abccdbc90ea89460217fb63d
Andrei Andronov <ao.andronov@gmail.com>
2022-06-11 12:43:51 +0300

    feat: adding logging

--- src/main/java/ru/andronov/Application.java
+++ src/main/java/ru/andronov/Application.java
@@ -1,6 +1,8 @@
 package ru.andronov;
 
 import lombok.SneakyThrows;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.andronov.services.InputMessageServiceImpl;
 
 import java.io.BufferedReader;
@@ -9,10 +11,12 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Application {
+    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
 
     @SneakyThrows
     public static void main(String[] args) {
         Socket socket = new Socket(Constants.HOST, Constants.PORT);
+        LOG.info("Connected to server");
 
         new Thread(new InputMessageServiceImpl(socket)).start();
 
